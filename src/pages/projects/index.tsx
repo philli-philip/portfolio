7d7b10409eeb35e9abdb4b5615350c1cb7dcca28
Philip Mattha <philip@mattha.net>
2023-03-28 11:36:23 +0200

    WIP: Add back Button which is a Link to Home page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Footer from "../../components/Footer";
 import Head from "next/head";
 import Image from "next/image";
+import LeftArrow from "../../components/icons/left-arrow";
+import Router from "next/router";
 
 const Category = ["product", "industrial", "code"] as const;
 type Category = (typeof Category)[number];
@@ -87,12 +89,25 @@ const Projects = () => {
   return (
     <>
       <Head>
-        <title>Projects</title>
+        <title>Projects - Philip Mattha</title>
       </Head>
       <main className="container mx-auto">
-        <h1 className="ml-8 mt-10 text-lg text-gray-700 dark:text-gray-300 md:mt-24 md:ml-0">
-          Projects
-        </h1>
+        <div className="dark:text-gray-3200 relative ml-8 mt-10 text-lg text-gray-700 md:mt-24 md:ml-0">
+          <Link
+            href="/"
+            className=" absolute hidden rounded-lg bg-gray-800 p-2 text-white shadow-lg hover:bg-gray-900 focus-visible:outline-orange-600 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600 md:block md:translate-y-[40px] md:-translate-x-12 lg:-translate-x-16 lg:translate-y-[36px] lg:p-3 xl:translate-y-[36px]"
+          >
+            <LeftArrow />
+          </Link>
+          <Link
+            href="/"
+            className="mr-1 -ml-1 inline hidden rounded p-1 text-gray-400 hover:bg-gray-100"
+          >
+            Home
+          </Link>
+          <span className="hidden pr-2 text-gray-400"></span>
+          <h1 className="inline">Projects</h1>
+        </div>
         <div className=" overflow-auto [@media(max-width:767px)]:scrollbar-hide">
           <div className="whitespace-nowrap pl-8 pr-16 md:pl-0">
             {filters.map((item, index) => (
