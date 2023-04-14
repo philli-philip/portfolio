bb6642bfcf6f04a55cf6f267d591ea16e1485dda
Philip Mattha <philip@mattha.net>
2023-04-14 18:19:10 +0200

    Feat: Add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/globals.css";
 import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Component {...pageProps}>
-      <Head>
-        <title>About Philip</title>
-        <meta name="description" content="Philip's CV" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </Component>
+    <>
+      <Component {...pageProps}>
+        <Head>
+          <title>About Philip</title>
+          <meta name="description" content="Philip's CV" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      </Component>
+      <Analytics />
+    </>
   );
 };
 
