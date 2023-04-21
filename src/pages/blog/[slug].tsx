2ca8565d1d217149f23fc3cbe6447a12f12c5fa1
Philip Mattha <philip@mattha.net>
2023-04-21 16:13:56 +0200

    Fix: Handle articles without category

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -83,7 +83,7 @@ const Article = ({ article }: { article: Article }) => {
                 format(Date.parse(article.publishedAt), "dd MMM yyy")}
           </span>
           <span>
-            {article?.categories.map((category, index) => {
+            {article?.categories?.map((category, index) => {
               return (
                 <span className="mr-2 " key={index}>
                   #{category}
