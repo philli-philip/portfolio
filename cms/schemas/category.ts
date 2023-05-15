333761cb7479fd79c22c4f42dac7e23f4bfb546f
Philip Mattha <philip@mattha.net>
2023-05-15 21:44:41 +0200

    Category slugs for project filtering & URL params

--- cms/schemas/category.ts
+++ cms/schemas/category.ts
@@ -15,5 +15,15 @@ export default defineType({
       title: 'Description',
       type: 'text',
     }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
   ],
 })
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,24 +1,32 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import Head from "next/head";
 import LeftArrow from "../../components/icons/left-arrow";
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import { getYear, parseJSON } from "date-fns";
 import sanityClient from "../../utils/sanity-client";
+import { useRouter } from "next/router";
 
 type Item = {
   title?: string;
   imageURL: string;
-  categories: string[];
+  categories: Slug[];
   publishedAt: Date;
   slug: string;
   imageAlt?: string;
 };
 
+type Slug = {
+  current: string;
+  title: string;
+  _id: string;
+};
+
 type Category = {
   title: string;
   _id: string;
+  slug: string;
 };
 
 export const getStaticProps: GetStaticProps<{
@@ -35,17 +43,21 @@ export const getStaticProps: GetStaticProps<{
         "imageAlt" : mainImage.caption,
         title,
         publishedAt,
-        "categories" :categories[] ->title,
+        "categories" :categories[] ->slug,
       } 
       | order(publishedAt desc)
     `
   );
 
-  const all: Category[] = [{ _id: "0", title: "All" }];
+  const all: Category[] = [{ _id: "0", slug: "all", title: "All" }];
   const categories = all.concat(
     await sanityClient.fetch(
       `
-      *[_type == "category"]
+      *[_type == "category"]{
+        "slug" : slug.current,
+        title,
+        _id
+      }
     `
     )
   );
@@ -63,7 +75,27 @@ const Projects = ({
   posts,
   categories,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [currentFilter, setCurrentFilter] = useState("All");
+  const router = useRouter();
+  console.log(router.query.project);
+  const [currentFilter, setCurrentFilter] = useState(
+    router.query.project || "all"
+  );
+
+  useEffect(() => {
+    const { project } = router.query;
+    setCurrentFilter(project);
+  }, [router.query]);
+
+  const filteredPosts = posts.filter((item) => {
+    if (currentFilter === "all") {
+      return true;
+    } else {
+      return item.categories.some(
+        (category) => category.current === currentFilter
+      );
+    }
+  });
+
   return (
     <>
       <Head>
@@ -92,11 +124,13 @@ const Projects = ({
               <button
                 key={index}
                 onClick={(e) => {
-                  setCurrentFilter(item.title);
-                  console.log(e);
+                  router.push("?project=" + item.slug, undefined, {
+                    shallow: true,
+                  });
+                  setCurrentFilter(item.slug);
                 }}
                 className={`my-2 py-2 pr-4 text-2xl font-bold md:mb-16 md:py-0 md:pr-6 md:text-5xl ${
-                  item.title === currentFilter
+                  item.slug === currentFilter
                     ? "text-gray-900 dark:text-gray-50"
                     : `text-gray-400 dark:text-gray-400`
                 }`}
@@ -107,37 +141,27 @@ const Projects = ({
           </div>
         </div>
         <div className="flex flex-wrap md:-mx-6">
-          {posts
-            .filter((item) => {
-              if (currentFilter === "All") {
-                return true;
-              } else if (item.categories.includes(currentFilter)) {
-                return true;
-              } else {
-                return false;
-              }
-            })
-            .map((item, index) => (
-              <Link
-                key={index}
-                href="#"
-                className="group relative mb-8 block aspect-video duration-75 sm:mb-0 sm:basis-1/2 sm:p-6 hover:sm:p-0 hover:sm:shadow-2xl lg:basis-1/3"
-              >
-                <img
-                  src={item.imageURL + "?w=980&fit=max"}
-                  alt={item.imageAlt && item.imageAlt}
-                  className="h-full w-full object-cover"
-                />
-                <div className="bottom-16 ml-2 mt-2 sm:absolute sm:bottom-6 sm:left-6 sm:hidden sm:group-hover:block">
-                  <h3 className="inline text-clip text-lg text-white mix-blend-exclusion sm:block">
-                    {item.title}
-                  </h3>
-                  <p className="ml-4 inline text-gray-400 mix-blend-exclusion sm:ml-0">
-                    {getYear(parseJSON(item.publishedAt))}
-                  </p>
-                </div>
-              </Link>
-            ))}
+          {filteredPosts.map((item, index) => (
+            <Link
+              key={index}
+              href="#"
+              className="group relative mb-8 block aspect-video duration-75 sm:mb-0 sm:basis-1/2 sm:p-6 hover:sm:p-0 hover:sm:shadow-2xl lg:basis-1/3"
+            >
+              <img
+                src={item.imageURL + "?w=980&fit=max"}
+                alt={item.imageAlt && item.imageAlt}
+                className="h-full w-full object-cover"
+              />
+              <div className="bottom-16 ml-2 mt-2 sm:absolute sm:bottom-6 sm:left-6 sm:hidden sm:group-hover:block">
+                <h3 className="inline text-clip text-lg text-white mix-blend-exclusion sm:block">
+                  {item.title}
+                </h3>
+                <p className="ml-4 inline text-gray-400 mix-blend-exclusion sm:ml-0">
+                  {getYear(parseJSON(item.publishedAt))}
+                </p>
+              </div>
+            </Link>
+          ))}
         </div>
       </main>
       <Footer />
