c84a7c509c4aff7b0e48011d42b8f992f6c19176
Philip Mattha <philip@mattha.net>
2023-08-14 14:33:58 +0200

    Feat: Code Page  dark mode and story book

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -9,15 +9,15 @@ type Props = {
 export default function Item(props:Props){
     const {name, difficulty, completed, id, link}  = props.task;
     return(
-        <a href={"/code/" + link} key={id} className="flex flex-row border-b border-gray-200 h-12 gap-2 md:gap-4 items-center px-4 md:px-6 justify-between hover:bg-gray-100 text-sm">
-            <span className=" text-gray-700 w-8">
+        <a href={"/code/" + link} key={id} className="flex flex-row border-b border-gray-200 dark:border-gray-700 h-12 gap-2 md:gap-4 items-center px-4 md:px-6 justify-between hover:bg-gray-100 dark:hover:bg-gray-700 text-sm">
+            <span className="text-secondary w-8">
                 {`P-` + id}
             </span>
-            <span className="font-semibold text-gray-800">
+            <span className="font-semibold text-primary">
                 {name}
             </span>
             <Status completed={completed}/>
-            <span className="text-gray-700 w-24 text-right flex flex-1 justify-end">
+            <span className="text-secondary w-24 text-right flex flex-1 justify-end">
                 {completed ? format(completed, "dd MMM YYY") : "--"}
             </span>
         </a>
--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -42,13 +42,13 @@ export default function Page() {
         />
       </Head>
       <div className="flex flex-row w-screen min-h-screen" onClick={()=>setOpen(false)}>
-      <nav className={`absolute w-9/12 bg-white border-r border-gray-200 h-screen px-6 py-3 duration-100 transition-all shadow-lg ${!open ? `opacity-0 -left-full`: `opacity-100 left-0`} md:relative md:w-48 md:block md:shadow-none md:opacity-100 md:left-0`}>
-        <span className="font-semibold pb-4 block">Philip Mattha</span>
+      <nav className={`absolute w-9/12 bg-white border-r dark:bg-gray-900 border-gray-200 dark:border-gray-700 h-screen px-6 py-3 duration-100 transition-all shadow-lg ${!open ? `opacity-0 -left-full`: `opacity-100 left-0`} md:relative md:w-48 md:block md:shadow-none md:opacity-100 md:left-0`}>
+        <span className="font-semibold pb-4 block text-gray-800 dark:text-gray-50">Philip Mattha</span>
         <ul>
           {navItems.map((item)=> {
             return(
               <li key={item.key}>
-                <a href={item.target} className="text-sm text-gray-700 block py-2">
+                <a href={item.target} className="text-sm text-gray-700 dark:text-gray-300 block py-2">
                   {item.label}
                 </a>
               </li>
@@ -57,9 +57,9 @@ export default function Page() {
         </ul>
       </nav>
       <main className="flex flex-grow flex-col">
-        <div className="flex flex-row h-12 border-b border-gray-200 items-center px-3 md:px-6 gap-4">
-          <button onClick={(event) => {event.stopPropagation();setOpen(true)}} className="md:hidden rounded-lg border p-2 shadow-sm border-gray-200"><Menu height={16} width={16}/></button>
-          <h1 className="flex">Code</h1>
+        <div className="flex flex-row h-12 border-b border-gray-200 dark:border-gray-700 items-center px-3 md:px-6 gap-4">
+          <button onClick={(event) => {event.stopPropagation();setOpen(true)}} className="md:hidden rounded-lg border p-2 shadow-sm border-gray-200 dark:border-gray-700 text-gray-700 dark:text-gray-200"><Menu height={16} width={16}/></button>
+          <h1 className="flex text-gray-800 dark:text-gray-200">Code</h1>
         </div>
       {tasks.map((item, index) => (
         <Item task={item} key={item.id}/>
