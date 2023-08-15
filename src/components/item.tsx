bf850c72bd94fdfdd84006a44e3ad314a3b26e29
Philip Mattha <philip@mattha.net>
2023-08-15 14:06:26 +0200

    Feat: Sort for Code Page

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,25 +1,26 @@
-import type { Task } from "../pages/code"
+import type { Task } from "../pages/code";
 import format from "date-fns-tz/format";
 import Status from "./completed";
 
 type Props = {
-    task:Task
-}
+  task: Task;
+};
 
-export default function Item(props:Props){
-    const {name, difficulty, completed, id, link}  = props.task;
-    return(
-        <a href={"/code/" + link} key={id} className="flex flex-row border-b border-gray-200 dark:border-gray-700 h-12 gap-2 md:gap-4 items-center px-4 md:px-6 justify-between hover:bg-gray-100 dark:hover:bg-gray-700 text-sm">
-            <span className="text-secondary w-8">
-                {`P-` + id}
-            </span>
-            <span className="font-semibold text-primary">
-                {name}
-            </span>
-            <Status completed={completed}/>
-            <span className="text-secondary w-24 text-right flex flex-1 justify-end">
-                {completed ? format(completed, "dd MMM YYY") : "--"}
-            </span>
-        </a>
-    )
+export default function Item(props: Props) {
+  const { name, difficulty, completed, id, link } = props.task;
+  return (
+    <a
+      href={"/code/" + link}
+      onClick={(e) => e.stopPropagation()}
+      key={id}
+      className="flex h-12 flex-row items-center justify-between gap-2 border-b border-gray-200 px-4 text-sm hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-700 md:gap-4 md:px-6"
+    >
+      <span className="text-secondary w-8">{`P-` + id}</span>
+      <span className="text-primary font-semibold">{name}</span>
+      <Status completed={completed} />
+      <span className="text-secondary flex w-24 flex-1 justify-end text-right">
+        {completed ? format(completed, "dd MMM YYY") : "--"}
+      </span>
+    </a>
+  );
 }
--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import { qrCode } from "./qr-code";
 import { adviceGenerator } from "./advice-generator";
 import { ClockApp } from "./clock-app";
@@ -7,30 +6,59 @@ import Item from "../../components/item";
 import { navItems } from "../../components/navBar";
 import { useState } from "react";
 import Menu from "../../components/icons/menu";
+import Settings from "../../components/icons/settings";
 
 export type Task = {
-  id: number,
+  id: number;
   name: string;
   link: string;
   difficulty: string;
   completed?: Date;
 };
 
-const code:Task = {
+const code: Task = {
   id: 4,
   name: "Code Page",
   difficulty: "intermediary",
-  link: "#"
+  link: "#",
+};
+
+const tasksList: Task[] = [];
+tasksList.push(qrCode);
+tasksList.push(adviceGenerator);
+tasksList.push(ClockApp);
+tasksList.push(code);
+
+function sortTasks(list: Task[], order: string) {
+  if (order === "name") {
+    return list.sort((a, b) => ("" + a.name).localeCompare(b.name));
+  } else if (order === "date") {
+    return list.sort((a, b) => {
+      if (a.completed === undefined) {
+        return 1;
+      } else if (b.completed === undefined) {
+        return -1;
+      } else {
+        return (
+          new Date(b.completed).getTime() - new Date(a.completed).getTime()
+        );
+      }
+    });
+  } else {
+    return list.sort((a, b) => b.id - a.id);
+  }
 }
 
-const tasks: Task[] = [];
-tasks.push(qrCode);
-tasks.push(adviceGenerator);
-tasks.push(ClockApp);
-tasks.push(code)
+const List = (props: { tasks: Task[]; ordering: string }) => {
+  return sortTasks(props.tasks, props.ordering).map((item) => (
+    <Item task={item} key={item.id} />
+  ));
+};
 
 export default function Page() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [settings, setSettings] = useState(false);
+  const [ordering, setOrdering] = useState("id");
 
   return (
     <>
@@ -41,30 +69,84 @@ export default function Page() {
           content="Some Code challenges I am working on next to the portfolio itself"
         />
       </Head>
-      <div className="flex flex-row w-screen min-h-screen" onClick={()=>setOpen(false)}>
-      <nav className={`absolute w-9/12 bg-white border-r dark:bg-gray-900 border-gray-200 dark:border-gray-700 h-screen px-6 py-3 duration-100 transition-all shadow-lg ${!open ? `opacity-0 -left-full`: `opacity-100 left-0`} md:relative md:w-48 md:block md:shadow-none md:opacity-100 md:left-0`}>
-        <span className="font-semibold pb-4 block text-gray-800 dark:text-gray-50">Philip Mattha</span>
-        <ul>
-          {navItems.map((item)=> {
-            return(
-              <li key={item.key}>
-                <a href={item.target} className="text-sm text-gray-700 dark:text-gray-300 block py-2">
-                  {item.label}
-                </a>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
-      <main className="flex flex-grow flex-col">
-        <div className="flex flex-row h-12 border-b border-gray-200 dark:border-gray-700 items-center px-3 md:px-6 gap-4">
-          <button onClick={(event) => {event.stopPropagation();setOpen(true)}} className="md:hidden rounded-lg border p-2 shadow-sm border-gray-200 dark:border-gray-700 text-gray-700 dark:text-gray-200"><Menu height={16} width={16}/></button>
-          <h1 className="flex text-gray-800 dark:text-gray-200">Code</h1>
-        </div>
-      {tasks.map((item, index) => (
-        <Item task={item} key={item.id}/>
-        ))}
-      </main>
+      <div
+        className="flex min-h-screen w-screen flex-row dark:bg-gray-900"
+        onClick={() => {
+          open && setOpen(false);
+          settings && setSettings(false);
+        }}
+      >
+        <nav
+          className={`absolute h-screen w-9/12 border-r border-gray-200 bg-white px-6 py-3 shadow-lg transition-all duration-100 dark:border-gray-700 dark:bg-gray-900 ${
+            !open ? `-left-full opacity-0` : `left-0 opacity-100`
+          } md:relative md:left-0 md:block md:w-48 md:opacity-100 md:shadow-none`}
+        >
+          <span className="block pb-4 font-semibold text-gray-800 dark:text-gray-50">
+            Philip Mattha
+          </span>
+          <ul>
+            {navItems.map((item) => {
+              return (
+                <li key={item.key}>
+                  <a
+                    href={item.target}
+                    className="block py-2 text-sm text-gray-700 dark:text-gray-300"
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <main className="flex flex-grow flex-col">
+          <div className="flex h-12 flex-row items-center gap-4 border-b border-gray-200 px-3 dark:border-gray-700 md:px-6">
+            <button
+              onClick={(event) => {
+                event.stopPropagation();
+                setOpen(!open);
+              }}
+              className="rounded-lg border border-gray-200 p-2 text-gray-700 shadow-sm dark:border-gray-700 dark:text-gray-200 md:hidden"
+            >
+              <Menu height={16} width={16} />
+            </button>
+            <h1 className="flex flex-1 text-gray-800 dark:text-gray-200">
+              Code
+            </h1>
+            <div className="relative inline-block text-left">
+              <div
+                className={`${
+                  settings ? `block` : `hidden`
+                } absolute right-0 top-8 z-10 origin-top-right rounded-lg bg-white py-2 shadow-lg dark:bg-gray-800`}
+              >
+                <div className="flex flex-row items-center px-4">
+                  <span className="text-secondary mr-8 text-sm">Ordering</span>
+                  <select
+                    className="border-default text-primary inline-block rounded-lg border bg-white py-1 pl-2 pr-2 text-sm hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
+                    name="ordering"
+                    defaultValue={ordering}
+                    onChange={(e) => setOrdering(e.target.value)}
+                  >
+                    <option value="id">ID</option>
+                    <option value="name">Name</option>
+                    <option value="date">Date</option>
+                  </select>
+                </div>
+              </div>
+              <button
+                onClick={(event) => {
+                  event.stopPropagation();
+                  setSettings(!settings);
+                }}
+                className="flex flex-1 items-center gap-2 rounded-lg border border-gray-200 px-2 py-1 text-sm text-gray-700 shadow-sm hover:bg-gray-100 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-700"
+              >
+                <Settings height={16} width={16} />
+                Settings
+              </button>
+            </div>
+          </div>
+          <List tasks={tasksList} ordering={ordering} />
+        </main>
       </div>
     </>
   );
