2a8447c6bf7ac7706125c3c7e2aaab836349e7cb
Philip Mattha <philip@mattha.net>
2023-11-18 14:38:57 +0100

    Fine tuning margins

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -16,11 +16,11 @@ export default function Page() {
     <body className=" bg-gray-100 dark:bg-gray-900">
       <NavBar current="/code" />
       <main className="min-h-screen">
-        <section className="mx-auto max-w-4xl pt-48">
+        <section className="mx-auto max-w-4xl pt-20 md:pt-32 lg:pt-48">
           <h1 className="px-4 text-3xl font-bold text-gray-800 dark:text-gray-200 md:px-8">
             Code
           </h1>
-          <p className="px-4 font-light text-gray-600 dark:text-gray-400 md:px-8">
+          <p className="px-4 pb-2 pt-2 font-light text-gray-600 dark:text-gray-400 md:px-8 md:pb-4 lg:pb-8">
             Past and current coding projects to refine my understanding of how
             the web works.
           </p>
