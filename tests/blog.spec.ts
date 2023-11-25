8f3d1991ed714ee9975ee979fb56f2a13dda8482
Philip Mattha <philip@mattha.net>
2023-11-25 21:03:45 +0100

    more tests

--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -2,4 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test("some blog post", async ({ page }) => {
   await page.goto("/blog");
+
+  expect(page.getByRole("button", { name: "All" })).toBeVisible();
 });
