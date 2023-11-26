25ddd116fd182a269f87c33c587d2d2b68dfb7df
Philip Mattha <philip@mattha.net>
2023-11-26 20:23:19 +0100

    More blog tests

--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -1,7 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test("some blog post", async ({ page }) => {
+test("Some blog post", async ({ page }) => {
   await page.goto("/blog");
 
-  expect(page.getByRole("button", { name: "All" })).toBeVisible();
+  await expect(page.getByRole("listitem")).toBeVisible();
+});
+
+test("Filter by category", async ({ page }) => {
+  await page.goto("/blog");
+  await page.getByRole("button", { name: "Opinion" }).click();
+  await expect(
+    page.getByText("Design Deliveries are not the goal")
+  ).toBeHidden();
+  await page.getByRole("button", { name: "All" }).click();
+  await expect(
+    page.getByText("Design Deliveries are not the goal")
+  ).toBeVisible();
 });
