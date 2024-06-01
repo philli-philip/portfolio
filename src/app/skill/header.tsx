37c1f622db37a7cafc1fc4b951399ec149b4811e
Philip Mattha <philip@mattha.net>
2024-06-02 00:02:22 +0200

    Tiny change for header

--- src/app/skill/header.tsx
+++ src/app/skill/header.tsx
@@ -4,9 +4,10 @@ export default function Header() {
   return (
     <Section>
       <h1 className="pt-12 text-2xl font-bold lg:text-4xl">Skill matrix</h1>
-      <p className="max-w-[50ch] pt-4 text-sm opacity-60">
+      <p className="max-w-[56ch] pt-4 text-sm opacity-60">
         Largely based on Figmas Design ladder. My addition is the guidance what
-        level you are based on the skills you have.
+        level you are based on the skills you have. Go through all section to
+        see what level you are.
       </p>
       <ol className="fley-row flex gap-x-8 pb-12 pt-4 text-sm opacity-60">
         <li>L1 — Early career</li>
